71ca541b59a7ce5f1c5386b0e09e56213eee4f5d
socie <charles-play@hotmail.fr>
2018-02-01 11:50:11 +0100

    commit sql version2

---
+++ src/TableSQL.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Produit(
+  IDPRODUIT  NUMBER NOT NULL ,
+  QUANTITESTOCK NUMBER(38),
+  NOM VARCHAR2(20),
+  PRIXUNITAIREHT FLOAT(63),
+  CONSTRAINT pk_idproduit PRIMARY KEY (IDPRODUIT));
+
+
+create PROCEDURE nouveauProduit (p_nom IN Produit.nom%TYPE,p_quantiteStock IN Produit.quantiteStock%TYPE,p_prixUnitaireHT IN Produit.prixUnitaireHT%TYPE)IS
+BEGIN
+  INSERT INTO Produit VALUES(seqProduit.nextval,p_quantiteStock,p_nom,p_prixUnitaireHT);
+END;
+
+REVOKE INSERT ON Produit FROM PUBLIC;
